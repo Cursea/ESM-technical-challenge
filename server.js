159a28f448b2f7ca4e187152ef0e91dd0a31862a
Marco <marcoburman@gmail.com>
2021-01-23 13:20:55 +0000

    Add routing; basic GET response

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = require('./app')
 const server = http.createServer(app)
 
 server.listen(3001, () => {
-  console.log(`Server running on port 3001`)
+  console.log(`Server running on port ${server.address().port}`)
 })
