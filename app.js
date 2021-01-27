9757dcf5c9bdc22124fa3e248dec98f1bcf54185
Marco <marcoburman@gmail.com>
2021-01-27 15:49:38 +0000

    Install cors dependency

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const routes = require('./routes')
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/', routes)
