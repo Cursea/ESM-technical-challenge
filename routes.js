4f281276e8dbedaa10ba99a777072c735f8b0646
Marco <marcoburman@gmail.com>
2021-01-27 19:50:20 +0000

    Attempt to fetch sheet data on /details get request

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const { request } = require('express')
 const express = require('express')
 const router = express.Router()
 const storage = require('./google-service')
+const fetch = require('node-fetch')
 
 router.get('/', (request, response) => {
   response.sendFile('public/index.html', { root: __dirname })
@@ -9,6 +10,16 @@ router.get('/', (request, response) => {
 
 router.get('/details', (request, response) => {
   response.sendFile('public/user-details.html', { root: __dirname })
+
+  const fetchData = async () => {
+    const data = await fetch(
+      `https://sheets.googleapis.com/v4/spreadsheets/1YxSQJtAqrW8ivFhU4ISgSr8-xIBEkXMLP-mTb1w2gtk/values/Sheet1!A2:C2`
+    )
+
+    const jsonData = await data.json()
+    return jsonData
+  }
+  console.log(fetchData())
 })
 
 router.post('/submit-form', (request, response) => {
