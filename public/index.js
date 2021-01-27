052cd2dd6a653833020a4502ba4617eb2a1c3800
Marco <marcoburman@gmail.com>
2021-01-27 11:48:49 +0000

    Prevent default form action; fetch to POST form data to server

---
+++ public/index.js
@@ -0,0 +1,22 @@
+document.forms['signupForm'].addEventListener('submit', (event) => {
+  event.preventDefault()
+  document.querySelector('#successMessage').classList.remove('hidden')
+
+  fetch(event.target.action, {
+    method: 'POST',
+    body: new URLSearchParams(new FormData(event.target)), // event.target is the form
+  })
+    .then((resp) => {
+      return resp.json() // or resp.text() or whatever the server sends
+    })
+    .then((body) => {
+      // TODO handle body
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+
+  document
+    .querySelectorAll('form input:not([type="submit"]')
+    .forEach((input) => (input.value = ''))
+})
